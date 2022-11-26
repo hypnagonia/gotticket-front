338b97aa9d6187966562cf38708320012da1776f
jenya <hypnagonia>
2022-11-26 18:02:42 +0400

    ++

--- src/gotticket/EventDetails.tsx
+++ src/gotticket/EventDetails.tsx
@@ -7,9 +7,15 @@ import {
   Link,
   useParams
 } from "react-router-dom";
-import { Box, DataTable, Text, Spinner, Tip, Card, CardHeader, CardFooter, Button, CardBody, Meter} from "grommet";
+import { Box, DataTable, Text, Spinner, Tip, Card,
+Table,
+TableBody,
+TableCell,
+TableRow,
+  CardHeader, CardFooter, Button, CardBody, Meter} from "grommet";
 import {useHistory} from "react-router-dom";
 import {BaseContainer} from './ui/BaseContainer'
+
 
 export const ticketColumns = [
   {
@@ -59,6 +65,14 @@ export function EventDetails(props: any) {
   const {id}: any = useParams();
   const history = useHistory();
 
+  const eventData = event ? [
+    {a:'When', b:event.eventDate},
+        {a:'Venue', b:event.venue.name},
+        {a:'Address', b:event.venue.address},
+        {a:'Description',b: event.description},
+        {a:'Promoter', b:event.company.name}
+  ] : null
+
   // @ts-ignore
   const tickets = event ? event.tickets : []
 
@@ -90,10 +104,24 @@ export function EventDetails(props: any) {
   const eventId = event ? event.id : null
   return (
     <BaseContainer pad="0">
-      <h2>Event <b>{event ? event!.name : id}</b></h2>
-      ({JSON.stringify(event)})
-      <br/><br/>
+      {event ? <h2 style={{textTransform: 'capitalize'}}><b>{event!.name}</b> at&nbsp;
+      <b>{event.venue.name} {event.venue.address}</b></h2> : null}
+<>
+{eventData ? <>
+<Box style={{ overflow: "auto", background: 'white'}} pad='medium'>
 
+<Table style={{maxWidth: 600, fontSize: 16}}>
+    <TableBody>
+        {eventData && eventData.map(datum => <TableRow>
+          <TableCell>{datum.a}</TableCell>
+          <TableCell style={{textTransform: 'capitalize'}}><b>{datum.b}</b></TableCell>
+        </TableRow>)}
+    </TableBody>
+  </Table>
+</Box>
+<br/>
+  </>: null}
+</>
       {
         <>
         {eventId && <Box align="start" pad="0">
@@ -104,10 +132,9 @@ export function EventDetails(props: any) {
               <DataTable
               className={"g-table-header"}
               style={{ width: "100%", minWidth: "620px" }}
+
               border={{
-                header: {
-                  color: "brand",
-                },
+
                 body: {
                   color: "border",
                   side: "top",
